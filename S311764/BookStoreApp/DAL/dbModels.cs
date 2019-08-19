921df10885d773632e035da97f631824d6a7fd14
chrishanpeiris <chrishanmx@gmail.com>
2019-08-19 19:33:55 +0930

    creating context

--- S311764/BookStoreApp/DAL/dbModels.cs
+++ S311764/BookStoreApp/DAL/dbModels.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStoreApp.DAL
 {
@@ -16,5 +17,15 @@ namespace BookStoreApp.DAL
         public int ID { get; set; }
         public string Name { get; set; }
         public string Brand { get; set; }
+    }
+
+    public class EfBridgeContext : DbContext
+    {
+        public EfBridgeContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Book> Book { get; set; }
+        public virtual DbSet<Printer> Printer{ get; set; }
     }
 }
