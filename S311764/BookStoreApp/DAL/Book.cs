1da3bef9fb93561a21694e805d14b2606a0efc95
chrishanpeiris <chrishanmx@gmail.com>
2019-08-19 14:34:38 +0930

    Creating DAL models

---
+++ S311764/BookStoreApp/DAL/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.DAL
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
