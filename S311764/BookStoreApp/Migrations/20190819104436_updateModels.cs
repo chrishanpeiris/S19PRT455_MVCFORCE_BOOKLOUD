ce336fa9bc6d983826fbde43a825b98a80ffd3c3
chrishanpeiris <chrishanmx@gmail.com>
2019-08-19 20:16:41 +0930

    Add migration - update models

---
+++ S311764/BookStoreApp/Migrations/20190819104436_updateModels.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStoreApp.Migrations
+{
+    public partial class updateModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Printer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Scanner",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    Brand = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scanner", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Scanner");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Printer");
+        }
+    }
+}
