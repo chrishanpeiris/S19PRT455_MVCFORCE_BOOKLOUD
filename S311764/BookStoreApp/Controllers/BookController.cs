4e0b3df9008b70ec91e33d2e81da7cf60802445f
chrishanpeiris <chrishanmx@gmail.com>
2019-08-19 23:05:33 +0930

    Add read operations to bookcontroller and create views

---
+++ S311764/BookStoreApp/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoreApp.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStoreApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly EfBridgeContext _db;
+
+        public BookController(EfBridgeContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Book.ToListAsync());
+        }
+    }
+}
