6cf29a82bd143d68ad9865390e3adba72d77557c
chrishanpeiris <chrishanmx@gmail.com>
2019-08-19 20:04:01 +0930

    Setup dependency injection

--- S311764/BookStoreApp/Startup.cs
+++ S311764/BookStoreApp/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStoreApp.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +35,8 @@ namespace BookStoreApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<EfBridgeContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("EfDbconnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
