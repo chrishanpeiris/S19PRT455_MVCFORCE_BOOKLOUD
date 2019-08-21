e51bf90ce7f0ea5dfd7de755c88cb673c95f9e27
Shih Liang Fang <37144635+haihau@users.noreply.github.com>
2019-08-21 13:18:22 +0930

    initialSetup

--- S308078/BookStoreApp/BookStoreApp/Migrations/20190821034735_updatedataase.Designer.cs
+++ S308078/BookStoreApp/BookStoreApp/Migrations/20190821034735_updatedataase.Designer.cs
@@ -3,14 +3,16 @@ using BookStoreApp.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookStoreApp.Migrations
 {
     [DbContext(typeof(EfBridgeContext))]
-    partial class EfBridgeContextModelSnapshot : ModelSnapshot
+    [Migration("20190821034735_updatedataase")]
+    partial class updatedataase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ S308078/BookStoreApp/BookStoreApp/Migrations/20190821034735_updatedataase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookStoreApp.Migrations
+{
+    public partial class updatedataase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Printer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Brand = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Printer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Printer");
+        }
+    }
+}
