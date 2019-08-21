5a52f26b075ee4fef8b48190824926bcac9af664
Shih Liang Fang <37144635+haihau@users.noreply.github.com>
2019-08-21 14:12:33 +0930

    Create a  Printer controller

---
+++ S308078/BookStoreApp/BookStoreApp/Controllers/PrintersController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookStoreApp.DAL;
+
+namespace BookStoreApp.Controllers
+{
+    public class PrintersController : Controller
+    {
+        private readonly EfBridgeContext _context;
+
+        public PrintersController(EfBridgeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Printers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Printer.ToListAsync());
+        }
+
+        // GET: Printers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var printer = await _context.Printer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (printer == null)
+            {
+                return NotFound();
+            }
+
+            return View(printer);
+        }
+
+        // GET: Printers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Printers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Brand,Model")] Printer printer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(printer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(printer);
+        }
+
+        // GET: Printers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var printer = await _context.Printer.FindAsync(id);
+            if (printer == null)
+            {
+                return NotFound();
+            }
+            return View(printer);
+        }
+
+        // POST: Printers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Brand,Model")] Printer printer)
+        {
+            if (id != printer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(printer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PrinterExists(printer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(printer);
+        }
+
+        // GET: Printers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var printer = await _context.Printer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (printer == null)
+            {
+                return NotFound();
+            }
+
+            return View(printer);
+        }
+
+        // POST: Printers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var printer = await _context.Printer.FindAsync(id);
+            _context.Printer.Remove(printer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PrinterExists(int id)
+        {
+            return _context.Printer.Any(e => e.Id == id);
+        }
+    }
+}
