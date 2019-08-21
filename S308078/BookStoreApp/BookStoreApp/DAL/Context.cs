fc431b3dc867aa0a02d22ae846b6212c50ea61a6
Shih Liang Fang <37144635+haihau@users.noreply.github.com>
2019-08-21 11:17:38 +0930

    Transfer from my repo

---
+++ S308078/BookStoreApp/BookStoreApp/DAL/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.DAL
+{
+    public class EfBridgeContext : DbContext
+    {
+        public EfBridgeContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+        public virtual DbSet<Printer> Printer { get; set; }
+
+    }
+
+}
