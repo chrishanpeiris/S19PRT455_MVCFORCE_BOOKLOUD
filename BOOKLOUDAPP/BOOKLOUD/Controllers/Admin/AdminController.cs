024a10858248e9e20614e08d352837ef5ae0e26f
chrishanpeiris <chrishanmx@gmail.com>
2019-08-22 17:12:28 +0930

    Initial setup, authenticaion and intial pages

---
+++ BOOKLOUDAPP/BOOKLOUD/Controllers/Admin/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BOOKLOUD.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserManagement()
+        {
+            return View();
+        }
+
+        public IActionResult UnitManagement()
+        {
+            return View();
+        }
+
+        public IActionResult Reports()
+        {
+            return View();
+        }
+    }
+}
