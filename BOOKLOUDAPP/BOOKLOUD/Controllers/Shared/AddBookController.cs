61f29c37c3a99f6282965d4fa6de7f6a9acfc5fc
chrishanpeiris <chrishanmx@gmail.com>
2019-09-04 16:06:38 +0930

    [BK_T20_Add_Books] add migration

---
+++ BOOKLOUDAPP/BOOKLOUD/Controllers/Shared/AddBookController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BOOKLOUD.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BOOKLOUD.Controllers.Shared
+{
+    public class AddBookController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public AddBookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost] //post method
+        public async Task<IActionResult> Create([Bind("Id, BookName, BookISBN")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(book); //add data to Book table
+                await _db.SaveChangesAsync(); //wait for database response
+            }
+
+            return RedirectToRoute("Default", new {Controller = "User", Action = "ListedBooks"});
+        }
+    }
+
+
+}
---
+++ BOOKLOUDAPP/BOOKLOUD/Data/Migrations/20190904063513_GenerateBookTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BOOKLOUD.Data.Migrations
+{
+    public partial class GenerateBookTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
