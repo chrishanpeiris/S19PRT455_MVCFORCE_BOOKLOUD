ba7a254f7edb25c15c885175b62ba07923b35305
Shih Liang Fang <37144635+haihau@users.noreply.github.com>
2019-09-04 15:59:16 +0930

    [BK_T20_Add_Books] Update book model to applicationdbcontext

--- BOOKLOUDAPP/BOOKLOUD/Data/ApplicationDbContext.cs
+++ BOOKLOUDAPP/BOOKLOUD/Data/ApplicationDbContext.cs
@@ -12,6 +12,10 @@ namespace BOOKLOUD.Data
         public int Id { get; set; }
         public string BookName { get; set; }
         public string BookAuthor { get; set; }
+        public string BookEdition { get; set; }
+        public string BookISBN { get; set; }
+        public string UniversityLocation { get; set; }
+        public string CourseName { get; set; }
 
     }
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
