b98f174eb6bcfe909530aa46ef093733eff146b5
chrishanpeiris <chrishanmx@gmail.com>
2019-09-09 00:02:37 +0930

    [BK_T22_Search_Books] add search functionality initial logic

--- BOOKLOUDAPP/BOOKLOUD/Controllers/User/SearchBookController.cs
+++ BOOKLOUDAPP/BOOKLOUD/Controllers/User/SearchBookController.cs
@@ -1,17 +1,41 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using BOOKLOUD.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BOOKLOUD.Controllers.User
 {
-    [Authorize(Roles = "Admin, User")]
     public class SearchBookController : Controller
     {
+        private ApplicationDbContext _db;
+
+        public SearchBookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         // GET: /<controller>/
-        public IActionResult Index()
+        /*public IActionResult Index()
         {
             return View();
+        }*/
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var books = from b in _db.Book
+                select b;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(s => s.BookName.Contains(searchString));
+            }
+
+            return View(await books.ToListAsync());
         }
     }
+
 }
