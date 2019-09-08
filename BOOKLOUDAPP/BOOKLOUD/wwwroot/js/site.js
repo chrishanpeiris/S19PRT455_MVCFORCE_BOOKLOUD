8d80bb91f602f6dd8e1009eb6c1581cd5ab01340
chrishanpeiris <chrishanmx@gmail.com>
2019-09-09 00:49:25 +0930

    [BK_T22_Search_Books] adding search autocomplete initial version + added jquery UI library

--- BOOKLOUDAPP/BOOKLOUD/wwwroot/js/site.js
+++ BOOKLOUDAPP/BOOKLOUD/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function() {
+    $("#BookName").autocomplete({
+        source: function(request, response) {
+            $.ajax({
+                url: "/SearchBook/AutoComplete",
+                type: "POST",
+                dataType: "json",
+                data: { Prefix: request.term },
+                success: function(data) {
+                    response($.map(data,
+                        function(item) {
+                            return { label: item.BookName, value: BookName };
+                        }))
+                }
+            })
+        },
+        messages: {
+            noResults: "",
+            results: ""
+        }
+    });
+})
