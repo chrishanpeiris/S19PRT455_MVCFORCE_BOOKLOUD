35c3bc2ac9c623867519e50e446cf905e5990221
Theshan <theshantsc@gmail.com>
2019-09-09 20:38:14 +0930

    [BK_T25_View_UserList] Adding async task to retrieve all users from user table + show usertype by injecting usermanager to the view

--- BOOKLOUDAPP/BOOKLOUD/Controllers/Admin/AdminPanelController.cs
+++ BOOKLOUDAPP/BOOKLOUD/Controllers/Admin/AdminPanelController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BOOKLOUD.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,15 +16,21 @@ namespace BOOKLOUD.Controllers.Admin
     [Authorize(Roles = "Admin")]
     public class AdminPanelController : Controller
     {
+        private ApplicationDbContext _db;
+
+        public AdminPanelController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult UserManagement()
+        public async Task<IActionResult> UserManagement()
         {
-            return View();
+            return View(await _db.Users.ToListAsync());
         }
 
         public IActionResult UserInfo()
