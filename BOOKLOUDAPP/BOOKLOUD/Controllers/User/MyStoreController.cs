4267613ed1005be959f0bf5f466115aa3a42d71f
Shih Liang Fang <37144635+haihau@users.noreply.github.com>
2019-09-10 20:22:27 +0930

    BK_T28_View_Book_Details - Update

--- BOOKLOUDAPP/BOOKLOUD/Controllers/User/MyStoreController.cs
+++ BOOKLOUDAPP/BOOKLOUD/Controllers/User/MyStoreController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BOOKLOUD.Data;
+using BOOKLOUD.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +17,12 @@ namespace BOOKLOUD.Controllers.User
     [Authorize(Roles = "Admin, User")]
     public class MyStoreController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public MyStoreController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -28,15 +37,20 @@ namespace BOOKLOUD.Controllers.User
         }
         public IActionResult PurchaseHistory()
         {
-
             return View();
         }
         public IActionResult MyBooks()
         {
             return View();
         }
-        public IActionResult ViewBooks(IFormFile pic)
+        public IActionResult ViewBooks()
         {
+            var bookinfo = _db.Book.OrderBy(t=>t.BookName).
+                Select(t => new ApplicationUser
+                {
+                    FirstName = t.BookName,
+                      
+                }).ToList();
             return View();
         }
 
