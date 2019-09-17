8b855bc6d13e7d237fd5bcf3c88a7229102176aa
Shih Liang Fang <37144635+haihau@users.noreply.github.com>
2019-09-17 23:19:48 +0930

    BK_T41_Edit_Books - Updated

--- BOOKLOUDAPP/BOOKLOUD/Controllers/User/MyStoreController.cs
+++ BOOKLOUDAPP/BOOKLOUD/Controllers/User/MyStoreController.cs
@@ -44,8 +44,16 @@ namespace BOOKLOUD.Controllers.User
         {
             return View(await _db.Book.ToListAsync());
         }
-        public async Task<IActionResult> ViewBooks()
+        public async Task<IActionResult> ViewBooks(string id)
         {
+            var books = from b in _db.Book
+                        select b;
+
+            if (!String.IsNullOrEmpty(id))
+            {
+                books = books.Where(s => s.BookName.Contains(id));
+            }
+
             return View();
         }
 
@@ -59,8 +67,63 @@ namespace BOOKLOUD.Controllers.User
             return View();
         }
 
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,BookName,BookAuthor,BookEdition,BookIsbn,UniversityLocation,CourseName,UnitName,BookPrice,BookDescription")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(book);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+        private bool BookExists(int id)
+        {
+            return _db.Book.Any(e => e.Id == id);
+        }
+
     }
 
 
-    
+
 }
