bbfba55e61e00699fddd44317e569401e679dbc2
theshantsc <theshantsc@gmail.com>
2019-09-19 15:30:06 +0930

    Create Add Unit page

--- BOOKLOUDAPP/BOOKLOUD/Controllers/Admin/AdminPanelController.cs
+++ BOOKLOUDAPP/BOOKLOUD/Controllers/Admin/AdminPanelController.cs
@@ -59,5 +59,10 @@ namespace BOOKLOUD.Controllers.Admin
         {
             return View();
         }
+
+        public IActionResult AddUnit()
+        {
+            return View();
+        }
     }
 }
