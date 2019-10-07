dbca1003047ca8c733d1282f4589afe1675e32c7
chrishanpeiris <chrishanmx@gmail.com>
2019-10-07 15:06:39 +0930

    [Week10_BK_T67_Add_Edit_Unit_Validation] - Add validation for required fields and set max values.

--- BOOKLOUDAPP/BOOKLOUD/Models/UnitDetailsModel.cs
+++ BOOKLOUDAPP/BOOKLOUD/Models/UnitDetailsModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,9 @@ namespace BOOKLOUD.Models
     public class UnitDetailsModel
     {
         public int Id { get; set; }
-
         public string UnitCode { get; set; }
-
         public string UnitName { get; set; }
-
         public virtual CourseDetailsModel Course { get; set; }
-
         public virtual UniversityDetailsModel University { get; set; }
 
     }
@@ -22,13 +19,15 @@ namespace BOOKLOUD.Models
     public class UnitDetailsViewModel
     {
         public int Id { get; set; }
-
+        [Required]
+        [StringLength(10, ErrorMessage = "Unit Code length can't be more than 10 letters.")]
         public string UnitCode { get; set; }
-
+        [Required]
+        [StringLength(25, ErrorMessage = "Unit Name length can't be more than 25 letters.")]
         public string UnitName { get; set; }
-
+        [Required]
         public int CourseId { get; set; }
-
+        [Required]
         public int UniversityId { get; set; }
 
     }
