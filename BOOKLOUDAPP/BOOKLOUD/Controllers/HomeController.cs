0936c2a157caea1b225d8a0d0e3c847dcf565d89
chrishanpeiris <chrishanmx@gmail.com>
2019-10-07 21:56:01 +0930

    [Week10_BK_T69_HomePage_Modifications] - Add featured top 10 books to the home page+ style changes

--- BOOKLOUDAPP/BOOKLOUD/Controllers/HomeController.cs
+++ BOOKLOUDAPP/BOOKLOUD/Controllers/HomeController.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using BOOKLOUD.Data;
 using Microsoft.AspNetCore.Mvc;
 using BOOKLOUD.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 
 namespace BOOKLOUD.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //ViewBag.Books = await _db.Book.ToListAsync();
+            //var test = _db.Book.Select(b => b.Id where);
+            IQueryable<Book> topTenResults = _db.Book.Take(10);
+            ViewBag.Books = topTenResults;
             return View();
         }
         [Authorize]
