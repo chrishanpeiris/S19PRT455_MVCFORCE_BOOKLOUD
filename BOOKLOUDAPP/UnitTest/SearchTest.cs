11cb647f607deebabb6c810f7449111d4ae4dff1
chrishanpeiris <chrishanmx@gmail.com>
2019-10-09 01:34:11 +0930

    [Week10_BK_T68_UnitTestForSearch] added unittest project + added unit test for search book controller

---
+++ BOOKLOUDAPP/UnitTest/SearchTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BOOKLOUD.Controllers.User;
+using BOOKLOUD.Data;
+using BOOKLOUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class SearchTest
+    {
+        private SearchBookController _searchBook;
+        private ApplicationDbContext _dbContext;
+
+        public SearchTest(SearchBookController searchBook, ApplicationDbContext dbContext)
+        {
+            _searchBook = searchBook;
+            _dbContext = dbContext;
+        }
+        [Test]
+        public async Task<IActionResult> Test_Search_With_Valid_Keyword()
+        {
+            var request = await _searchBook.Index("iot");
+            Assert.AreEqual("iot", request);
+
+            return null;
+        }
+
+        [Ignore("Ignore Test")]
+        public void Test_To_Ignore()
+        {
+
+        }
+    }
+}
